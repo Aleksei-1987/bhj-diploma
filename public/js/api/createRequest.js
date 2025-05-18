df2c3e53e12c9d3df47fe62b90a71c6a45270c0f
Aleksei Barkasov <alekseibarkasov@gmail.com>
2025-05-18 12:16:49 +0300

    created createRequest

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -3,5 +3,37 @@
  * на сервер.
  * */
 const createRequest = (options = {}) => {
+  const request = new XMLHttpRequest();
 
+  let requestData = options.data;
+
+  request.responseType = "json";
+
+  if (options.method === "GET") {
+    const params = new URLSearchParams();
+    for (const key in requestData) {
+      params.append(key, requestData[key]);
+    }
+    options.url += "?" + params.toString();
+  } else {
+    requestData = new FormData();
+    for (const key in options.data) {
+      requestData.append(key, options.data[key]);
+    }
+  }
+
+  request.onload = () => {
+    if (request.status >= 200 && request.status < 300) {
+      options.callback(null, request.response);
+    }
+  };
+
+  request.open(options.method, options.url, true);
+  try {
+    request.send(requestData);
+  } catch (err) {
+    options.callback(err, null);
+  }
+
+  return request;
 };

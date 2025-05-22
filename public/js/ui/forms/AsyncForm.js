7275af543b5ef5db0f2c0f6e118a182bee938d7b
Aleksei Barkasov <alekseibarkasov@gmail.com>
2025-05-22 19:46:44 +0300

    add AsyncForm

--- public/js/ui/forms/AsyncForm.js
+++ public/js/ui/forms/AsyncForm.js
@@ -13,7 +13,12 @@ class AsyncForm {
    * через registerEvents()
    * */
   constructor(element) {
+    if (!element) {
+      throw new Error("Элемент не существует");
+    }
 
+    this.element = element;
+    this.registerEvents();
   }
 
   /**
@@ -21,7 +26,10 @@ class AsyncForm {
    * вызывает метод submit()
    * */
   registerEvents() {
-
+    this.element.addEventListener("submit", (e) => {
+      e.preventDefault();
+      this.submit();
+    });
   }
 
   /**
@@ -32,18 +40,26 @@ class AsyncForm {
    * }
    * */
   getData() {
+    const form = this.element.querySelector("form");
+    const formData = new FormData(form);
+    entries = formData.entries();
+    const result = {};
 
+    for (let item of entries) {
+      result[item[0]] = item[1];
+    }
+
+    return result;
   }
 
-  onSubmit(options){
-
-  }
+  onSubmit(options) {}
 
   /**
    * Вызывает метод onSubmit и передаёт туда
    * данные, полученные из метода getData()
    * */
   submit() {
-
+    const data = this.getData();
+    this.onSubmit(data);
   }
 }
